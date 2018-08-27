ae247a60d2ef06b30ac5a8b8b87f99772655ca0b
Divya <divyamohanan@gmail.com>
2018-08-27 16:40:40 -0700

    "Upgrading Dates works"

--- lib/trip.rb
+++ lib/trip.rb
@@ -1,4 +1,6 @@
 require 'csv'
+require 'time'
+require 'pry'
 
 module RideShare
   class Trip
@@ -15,6 +17,9 @@ module RideShare
       if @rating > 5 || @rating < 1
         raise ArgumentError.new("Invalid rating #{@rating}")
       end
+      if @start_time > @end_time
+        raise ArgumentError.new("Start time greater")
+      end
     end
 
     def inspect
@@ -22,5 +27,22 @@ module RideShare
       "ID=#{id.inspect} " +
       "PassengerID=#{passenger&.id.inspect}>"
     end
+
+    #Add an instance method to the Trip class to calculate the duration of the trip in seconds, and a corresponding test
+    def duration_of_trip
+      return @end_time - @start_time
+    end
   end
 end
+
+
+# trip_data = {
+#   id: 8,
+#   passenger: "Div",
+#   start_time: "2018-06-07 04:18:47 -0700",
+#   end_time: "2018-06-07 04:19:25 -0700",
+#   cost: 23.45,
+#   rating: 3
+# }
+# trip = RideShare::Trip.new(trip_data)
+# trip.duration_of_trip
