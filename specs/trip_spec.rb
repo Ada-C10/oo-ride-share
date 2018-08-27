7153c22b9158d092d9cad6d8698a298bf3a26c4e
Hayden Williams <hwilliams.deva@gmail.com>
2018-08-27 15:25:40 -0700

    Add test in Trip class for #duration instance method

--- specs/trip_spec.rb
+++ specs/trip_spec.rb
@@ -59,4 +59,25 @@ describe "Trip class" do
       }.must_raise ArgumentError
     end
   end
+
+  describe "#duration method" do
+
+    it "Finds difference in seconds between end_time and start_time" do
+      start_time = Time.parse('2015-05-20T12:14:00+00:00')
+      end_time = start_time + 25 * 60 # 25 minutes
+      @trip_data = {
+        id: 8,
+        passenger: RideShare::User.new(id: 1,
+                                       name: "Ada",
+                                       phone: "412-432-7640"),
+        start_time: start_time,
+        end_time: end_time,
+        cost: 23.45,
+        rating: 3
+      }
+      @trip = RideShare::Trip.new(@trip_data)
+
+      expect(@trip.duration).must_equal 1500
+    end
+  end
 end
