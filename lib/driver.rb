52e2ee7b3634c1bf1fb07fda84a58aaa42992547
Aurea <fu.sakura@gmail.com>
2018-08-27 15:50:58 -0700

    create driver class and initialize method

---
+++ lib/driver.rb
@@ -0,0 +1,30 @@
+module RideShare
+
+  class Driver < User
+
+    attr_reader :vehicle_id, :driven_trips, :status
+
+    def initialize(input)
+      super(input)
+
+
+
+      @vehicle_id = input[:vin]
+      @driven_trips = input[:driven_trips].nil? ? [] : input[:driven_trips]
+      @status = :AVAILABLE
+
+      unless @vehicle_id.length == 17
+        raise ArgumentError, "Invalid vehicle ID #{@vehicle_id}"
+      end
+
+      unless [:AVAILABLE, :UNAVAILABLE].include?(@status)
+        raise ArgumentError, "Invalid status #{@status}"
+      end
+
+      
+    end
+
+
+  end
+
+end
