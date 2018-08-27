37b5c5f56aec5e66504c6801739b221348056339
Madaleine Shields <maddiev.shields@gmail.com>
2018-08-27 15:04:35 -0700

    Added Argument Error for end time being before start time

--- lib/trip.rb
+++ lib/trip.rb
@@ -1,4 +1,5 @@
 require 'csv'
+require 'pry'
 
 module RideShare
   class Trip
@@ -15,6 +16,11 @@ module RideShare
       if @rating > 5 || @rating < 1
         raise ArgumentError.new("Invalid rating #{@rating}")
       end
+
+      if @end_time < @start_time
+        raise ArgumentError.new("Start time cannot be greater than End time")
+      end
+
     end
 
     def inspect
