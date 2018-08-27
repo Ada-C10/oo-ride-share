30ef48bdcc085010d7141746ec3f3cf30ac857cf
Anibel America <ameriabel@gmail.com>
2018-08-27 15:07:57 -0700

    initialize method raises an argument if start time occurs after end time

--- lib/trip.rb
+++ lib/trip.rb
@@ -12,6 +12,10 @@ module RideShare
       @cost = input[:cost]
       @rating = input[:rating]
 
+      if @end_time < @start_time
+        raise ArgumentError, "End time is before start time"
+      end
+
       if @rating > 5 || @rating < 1
         raise ArgumentError.new("Invalid rating #{@rating}")
       end
