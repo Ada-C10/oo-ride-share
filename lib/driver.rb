b157fdd0c8d68194f3256f05120c6d5ec9743607
Hayden Williams <hwilliams.deva@gmail.com>
2018-08-27 16:31:32 -0700

    Implement Driver class constructor method and exceptions

--- lib/driver.rb
+++ lib/driver.rb
@@ -1,12 +1,20 @@
 require_relative 'user'
 
-class Driver < User
-  def initialize(input)
-    super(input)
-    @vehicle_id = input[:vehicle_id]
-    @driven_trips = input[:driven_trips]
-    @status = input[:status]
+module RideShare
+  class Driver < RideShare::User
+    attr_reader :vehicle_id, :driven_trips, :status
+
+    def initialize(input)
+      super(input)
+      @vehicle_id = input[:vin]
+      @driven_trips = input[:driven_trips]
+      @status = input[:status]
+
+      raise ArgumentError.new unless [:AVAILABLE, :UNAVAILABLE].include?(@status)
+
+      raise ArgumentError.new unless @vehicle_id.length == 17
+
+    end
 
   end
-
 end
