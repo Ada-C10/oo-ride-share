7b7c68cd74bc0a941a26afef9606d6a542be1456
Jane <jane.edwards.mcnear@gmail.com>
2018-08-27 16:00:21 -0700

    Updated tests for: raises error if end time is before start time

--- specs/trip_dispatch_spec.rb
+++ specs/trip_dispatch_spec.rb
@@ -8,107 +8,113 @@ describe "TripDispatcher class" do
   describe "Initializer" do
     it "is an instance of TripDispatcher" do
       dispatcher = RideShare::TripDispatcher.new(USER_TEST_FILE,
-                                                 TRIP_TEST_FILE)
-      expect(dispatcher).must_be_kind_of RideShare::TripDispatcher
-    end
-
-    it "establishes the base data structures when instantiated" do
-      dispatcher = RideShare::TripDispatcher.new
-      [:trips, :passengers].each do |prop|
-        expect(dispatcher).must_respond_to prop
+        TRIP_TEST_FILE)
+        expect(dispatcher).must_be_kind_of RideShare::TripDispatcher
       end
 
-      expect(dispatcher.trips).must_be_kind_of Array
-      expect(dispatcher.passengers).must_be_kind_of Array
-      # expect(dispatcher.drivers).must_be_kind_of Array
-    end
-  end
+      it "establishes the base data structures when instantiated" do
+        dispatcher = RideShare::TripDispatcher.new
+        [:trips, :passengers].each do |prop|
+          expect(dispatcher).must_respond_to prop
+        end
 
-  describe "find_user method" do
-    before do
-      @dispatcher = RideShare::TripDispatcher.new
-    end
+        expect(dispatcher.trips).must_be_kind_of Array
+        expect(dispatcher.passengers).must_be_kind_of Array
+        # expect(dispatcher.drivers).must_be_kind_of Array
+      end
+      it "raises error if end time is before start time" do
 
-    it "throws an argument error for a bad ID" do
-      expect{ @dispatcher.find_passenger(0) }.must_raise ArgumentError
-    end
+        test_trip_input = RideShare::Trip.new({:id => 1234, :passenger => "George Foreman", :start_time => "2018-05-25 12:25:00 -0700", :end_time => "2018-05-25 11:52:40 -0700", :cost => 100, :rating => 3})
 
-    it "finds a user instance" do
-      passenger = @dispatcher.find_passenger(2)
-      expect(passenger).must_be_kind_of RideShare::User
-    end
-  end
-
-
-  # Uncomment for Wave 2
-  # describe "find_driver method" do
-  #   before do
-  #     @dispatcher = RideShare::TripDispatcher.new
-  #   end
-  #
-  #   it "throws an argument error for a bad ID" do
-  #     expect { @dispatcher.find_driver(0) }.must_raise ArgumentError
-  #   end
-  #
-  #   it "finds a driver instance" do
-  #     driver = @dispatcher.find_driver(2)
-  #     expect(driver).must_be_kind_of RideShare::Driver
-  #   end
-  # end
-
-  describe "Driver & Trip loader methods" do
-    before do
-      @dispatcher = RideShare::TripDispatcher.new(USER_TEST_FILE,
-                                                 TRIP_TEST_FILE)
-    end
-
-    it "accurately loads driver information into drivers array" do
-      skip # Unskip After Wave 2
-      first_driver = @dispatcher.drivers.first
-      last_driver = @dispatcher.drivers.last
-
-      expect(first_driver.name).must_equal "Driver2"
-      expect(first_driver.id).must_equal 2
-      expect(first_driver.status).must_equal :UNAVAILABLE
-      expect(last_driver.name).must_equal "Driver8"
-      expect(last_driver.id).must_equal 8
-      expect(last_driver.status).must_equal :AVAILABLE
-    end
-
-    it "Connects drivers with trips" do
-      skip # Unskip after wave 2
-      trips = @dispatcher.trips
-
-      [trips.first, trips.last].each do |trip|
-        driver = trip.driver
-        expect(driver).must_be_instance_of RideShare::Driver
-        expect(driver.trips).must_include trip
+        expect{ test_trip_input }.must_raise ArgumentError
       end
     end
-  end
 
-  describe "User & Trip loader methods" do
-    before do
-      @dispatcher = RideShare::TripDispatcher.new(USER_TEST_FILE,
-                                                  TRIP_TEST_FILE)
+    describe "find_user method" do
+      before do
+        @dispatcher = RideShare::TripDispatcher.new
+      end
+
+      it "throws an argument error for a bad ID" do
+        expect{ @dispatcher.find_passenger(0) }.must_raise ArgumentError
+      end
+
+      it "finds a user instance" do
+        passenger = @dispatcher.find_passenger(2)
+        expect(passenger).must_be_kind_of RideShare::User
+      end
     end
 
-    it "accurately loads passenger information into passengers array" do
-      first_passenger = @dispatcher.passengers.first
-      last_passenger = @dispatcher.passengers.last
 
-      expect(first_passenger.name).must_equal "User1"
-      expect(first_passenger.id).must_equal 1
-      expect(last_passenger.name).must_equal "Driver8"
-      expect(last_passenger.id).must_equal 8
-    end
+    # Uncomment for Wave 2
+    # describe "find_driver method" do
+    #   before do
+    #     @dispatcher = RideShare::TripDispatcher.new
+    #   end
+    #
+    #   it "throws an argument error for a bad ID" do
+    #     expect { @dispatcher.find_driver(0) }.must_raise ArgumentError
+    #   end
+    #
+    #   it "finds a driver instance" do
+    #     driver = @dispatcher.find_driver(2)
+    #     expect(driver).must_be_kind_of RideShare::Driver
+    #   end
+    # end
 
-    it "accurately loads trip info and associates trips with passengers" do
-      trip = @dispatcher.trips.first
-      passenger = trip.passenger
+    describe "Driver & Trip loader methods" do
+      before do
+        @dispatcher = RideShare::TripDispatcher.new(USER_TEST_FILE,
+          TRIP_TEST_FILE)
+        end
 
-      expect(passenger).must_be_instance_of RideShare::User
-      expect(passenger.trips).must_include trip
-    end
-  end
-end
+        it "accurately loads driver information into drivers array" do
+          skip # Unskip After Wave 2
+          first_driver = @dispatcher.drivers.first
+          last_driver = @dispatcher.drivers.last
+
+          expect(first_driver.name).must_equal "Driver2"
+          expect(first_driver.id).must_equal 2
+          expect(first_driver.status).must_equal :UNAVAILABLE
+          expect(last_driver.name).must_equal "Driver8"
+          expect(last_driver.id).must_equal 8
+          expect(last_driver.status).must_equal :AVAILABLE
+        end
+
+        it "Connects drivers with trips" do
+          skip # Unskip after wave 2
+          trips = @dispatcher.trips
+
+          [trips.first, trips.last].each do |trip|
+            driver = trip.driver
+            expect(driver).must_be_instance_of RideShare::Driver
+            expect(driver.trips).must_include trip
+          end
+        end
+      end
+
+      describe "User & Trip loader methods" do
+        before do
+          @dispatcher = RideShare::TripDispatcher.new(USER_TEST_FILE,
+            TRIP_TEST_FILE)
+          end
+
+          it "accurately loads passenger information into passengers array" do
+            first_passenger = @dispatcher.passengers.first
+            last_passenger = @dispatcher.passengers.last
+
+            expect(first_passenger.name).must_equal "User1"
+            expect(first_passenger.id).must_equal 1
+            expect(last_passenger.name).must_equal "Driver8"
+            expect(last_passenger.id).must_equal 8
+          end
+
+          it "accurately loads trip info and associates trips with passengers" do
+            trip = @dispatcher.trips.first
+            passenger = trip.passenger
+
+            expect(passenger).must_be_instance_of RideShare::User
+            expect(passenger.trips).must_include trip
+          end
+        end
+      end
