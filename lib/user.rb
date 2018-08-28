dc817e48a36857871174b28f0f18fd695f18eca5
xLJohnsonx <lizjohnsonemail@gmail.com>
2018-08-28 13:59:48 -0700

    method built to pass total time spent on all trips for user - wave 1 complete

--- lib/user.rb
+++ lib/user.rb
@@ -29,7 +29,15 @@ module RideShare
     # Add an instance method, total_time_spent to User that will return
     # the total amount of time that user has spent on their trips
     def total_time_spent
-
+      total_time = 0
+      @trips.each do |trip_inst|
+        total_time += (trip_inst.end_time - trip_inst.start_time)
+      end
+      # TODO: implement Time.parser to convert seconds to minutes
+      # t = 236 # seconds
+      # Time.at(t).utc.strftime("%H:%M:%S")
+      # => "00:03:56"
+      return total_time
     end
   end
 end
