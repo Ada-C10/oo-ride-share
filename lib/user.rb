99c9cae1c567dd20d5b6d8138e67312610455af6
jackie <jacquelyn.cheng@me.com>
2018-08-28 13:48:40 -0700

    Modified spec_helper.rb to include driver.rb

--- lib/user.rb
+++ lib/user.rb
@@ -26,4 +26,20 @@ module RideShare
       return trips.reduce(0) {|sum, trip| sum + trip.duration}
     end
   end
+
+  # class Driver < User
+  #   attr_reader :vehicle_id, :driven_trips, :status
+  #
+  #   def initialize(input)
+  #     super(input)
+  #     if [:AVAILABLE, :UNAVAILABLE].include? input[:status]
+  #       @status = status
+  #     else
+  #       raise ArgumentError, "Invalid status, must be :AVAILABLE or :UNAVAILABLE"
+  #     end
+  #     @vehicle_id = input[:vin]
+  #     @driven_trips = []
+  #   end
+  #
+  # end
 end
