efe12e725741f0877339d1fc953a6651b4ccb697
jackie <jacquelyn.cheng@me.com>
2018-08-28 13:49:13 -0700

    removed duplicated code from user.rb

--- lib/user.rb
+++ lib/user.rb
@@ -27,19 +27,4 @@ module RideShare
     end
   end
 
-  # class Driver < User
-  #   attr_reader :vehicle_id, :driven_trips, :status
-  #
-  #   def initialize(input)
-  #     super(input)
-  #     if [:AVAILABLE, :UNAVAILABLE].include? input[:status]
-  #       @status = status
-  #     else
-  #       raise ArgumentError, "Invalid status, must be :AVAILABLE or :UNAVAILABLE"
-  #     end
-  #     @vehicle_id = input[:vin]
-  #     @driven_trips = []
-  #   end
-  #
-  # end
 end
