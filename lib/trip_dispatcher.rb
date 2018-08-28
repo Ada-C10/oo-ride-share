ed439c9e78ecaac8bedc48fa1689b742471b48e6
Madaleine Shields <maddiev.shields@gmail.com>
2018-08-28 16:49:33 -0700

    Add a driver to trip_data in the load_trips method and restructured initialize method

--- lib/trip_dispatcher.rb
+++ lib/trip_dispatcher.rb
@@ -9,63 +9,66 @@ module RideShare
   class TripDispatcher
     attr_reader :drivers, :passengers, :trips
 
-    def initialize(user_file = 'support/users.csv',
-                   trip_file = 'support/trips.csv',
-                   driver_file = 'support/drivers.csv')
-      @passengers = load_users(user_file)
-      @trips = load_trips(trip_file)
-      @drivers = load_drivers(driver_file)
+    def initialize(
+      user_file = 'support/users.csv',
+      driver_file = 'support/drivers.csv',
+      trip_file = 'support/trips.csv'
+    )
+    @passengers = load_users(user_file)
+    @drivers = load_drivers(driver_file)
+    @trips = load_trips(trip_file)
+  end
+
+  def load_drivers(filename)
+    drivers = []
+
+    CSV.read(filename, headers: true).each do |line|
+      passenger = @passengers.find { |passenger| passenger.id == line[0].to_i }
+
+      input_data = {}
+      input_data[:id] = line[0].to_i
+      input_data[:vin] = line[1]
+      input_data[:status] = line[2].to_sym
+      input_data[:name] = passenger.name
+
+      driver = Driver.new(input_data)
+      drivers << driver
     end
+    return drivers
+  end
 
-    def load_drivers(filename)
-      drivers = []
+  def find_driver(id)
+    check_id(id)
+    return @drivers.find { |driver| driver.id == id }
+  end
 
-      CSV.read(filename, headers: true).each do |line|
-        passenger = @passengers.find { |passenger| passenger.id == line[0].to_i }
+  def load_users(filename)
+    users = []
 
-        input_data = {}
-        input_data[:id] = line[0].to_i
-        input_data[:vin] = line[1]
-        input_data[:status] = line[2].to_sym
-        input_data[:name] = passenger.name
+    CSV.read(filename, headers: true).each do |line|
+      input_data = {}
+      input_data[:id] = line[0].to_i
+      input_data[:name] = line[1]
+      input_data[:phone] = line[2]
 
-        driver = Driver.new(input_data)
-        drivers << driver
-      end
-      return drivers
+      users << User.new(input_data)
     end
-
-    def find_driver(id)
-      check_id(id)
-      return @drivers.find { |driver| driver.id == id }
-    end
-
-    def load_users(filename)
-      users = []
-
-      CSV.read(filename, headers: true).each do |line|
-        input_data = {}
-        input_data[:id] = line[0].to_i
-        input_data[:name] = line[1]
-        input_data[:phone] = line[2]
-
-        users << User.new(input_data)
-      end
-
-      return users
-    end
+    return users
+  end
 
 
-    def load_trips(filename)
-      trips = []
-      trip_data = CSV.open(filename, 'r', headers: true,
-                                          header_converters: :symbol)
+  def load_trips(filename)
+    trips = []
+    trip_data = CSV.open(filename, 'r', headers: true,
+      header_converters: :symbol)
 
       trip_data.each do |raw_trip|
         passenger = find_passenger(raw_trip[:passenger_id].to_i)
+        driver = find_driver(raw_trip[:driver_id].to_i)
 
         parsed_trip = {
           id: raw_trip[:id].to_i,
+          driver: driver,
           passenger: passenger,
           start_time: Time.parse(raw_trip[:start_time]),
           end_time: Time.parse(raw_trip[:end_time]),
@@ -88,9 +91,9 @@ module RideShare
 
     def inspect
       return "#<#{self.class.name}:0x#{self.object_id.to_s(16)} \
-              #{trips.count} trips, \
-              #{drivers.count} drivers, \
-              #{passengers.count} passengers>"
+      #{trips.count} trips, \
+      #{drivers.count} drivers, \
+      #{passengers.count} passengers>"
     end
 
     private
