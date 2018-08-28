f536ff41d74f742b665a1ebc42de8ef7a113f10d
Leanne Rivera <rivera.leanne+ada@gmail.com>
2018-08-28 15:51:51 -0700

    Driver class created, tests passed.

---
+++ lib/driver.rb
@@ -0,0 +1,21 @@
+require 'user'
+module RideShare
+  class Driver < RideShare::User
+    attr_reader :vin, :vehicle_id, :driven_trips, :status
+
+    def initialize(input)
+      super(input)
+
+      if input[:vin].nil? || input[:vin].length != 17
+        raise ArgumentError, 'VIN cannot be blank or have letters.'
+      end
+
+      @vehicle_id = ""
+
+      @vin = input[:vin]
+      @vehicle_id ||= input[:vehicle_id]
+      @driven_trips = input[:driven_trips]
+      @status = input[:status]
+    end
+  end
+end
--- specs/driver_spec.rb
+++ specs/driver_spec.rb
@@ -1,6 +1,6 @@
 require_relative 'spec_helper'
 
-xdescribe "Driver class" do
+describe "Driver class" do
 
   describe "Driver instantiation" do
     before do
@@ -40,65 +40,65 @@ xdescribe "Driver class" do
     end
   end
 
-  describe "add_driven_trip method" do
-    before do
-      pass = RideShare::User.new(id: 1, name: "Ada", phone: "412-432-7640")
-      @driver = RideShare::Driver.new(id: 3, name: "Lovelace", vin: "12345678912345678")
-      @trip = RideShare::Trip.new({id: 8, driver: @driver, passenger: pass, date: "2016-08-08", rating: 5})
-    end
-
-    it "throws an argument error if trip is not provided" do
-      expect{ @driver.add_trip(1) }.must_raise ArgumentError
-    end
-
-    it "increases the trip count by one" do
-      previous = @driver.trips.length
-      @driver.add_trip(@trip)
-      expect(@driver.trips.length).must_equal previous + 1
-    end
-  end
-
-  describe "average_rating method" do
-    before do
-      @driver = RideShare::Driver.new(id: 54, name: "Rogers Bartell IV",
-                                      vin: "1C9EVBRM0YBC564DZ")
-      trip = RideShare::Trip.new(id: 8, driver: @driver, passenger: nil,
-                                 date: Time.parse("2016-08-08"), rating: 5)
-      @driver.add_trip(trip)
-    end
-
-    it "returns a float" do
-      expect(@driver.average_rating).must_be_kind_of Float
-    end
-
-    it "returns a float within range of 1.0 to 5.0" do
-      average = @driver.average_rating
-      expect(average).must_be :>=, 1.0
-      expect(average).must_be :<=, 5.0
-    end
-
-    it "returns zero if no trips" do
-      driver = RideShare::Driver.new(id: 54, name: "Rogers Bartell IV",
-                                     vin: "1C9EVBRM0YBC564DZ")
-      expect(driver.average_rating).must_equal 0
-    end
-
-    it "correctly calculates the average rating" do
-      trip2 = RideShare::Trip.new(id: 8, driver: @driver, passenger: nil,
-                                  date: Time.parse("2016-08-08"), rating: 1)
-      @driver.add_trip(trip2)
-
-      expect(@driver.average_rating).must_be_close_to (5.0 + 1.0) / 2.0, 0.01
-    end
-
-
-  end
-
-  describe "total_revenue" do
-    # You add tests for the total_revenue method
-  end
-
-  describe "net_expenditures" do
-    # You add tests for the net_expenditures method
-  end
+  # describe "add_driven_trip method" do
+  #   before do
+  #     pass = RideShare::User.new(id: 1, name: "Ada", phone: "412-432-7640")
+  #     @driver = RideShare::Driver.new(id: 3, name: "Lovelace", vin: "12345678912345678")
+  #     @trip = RideShare::Trip.new({id: 8, driver: @driver, passenger: pass, date: "2016-08-08", rating: 5})
+  #   end
+  #
+  #   it "throws an argument error if trip is not provided" do
+  #     expect{ @driver.add_trip(1) }.must_raise ArgumentError
+  #   end
+  #
+  #   it "increases the trip count by one" do
+  #     previous = @driver.trips.length
+  #     @driver.add_trip(@trip)
+  #     expect(@driver.trips.length).must_equal previous + 1
+  #   end
+  # end
+  #
+  # describe "average_rating method" do
+  #   before do
+  #     @driver = RideShare::Driver.new(id: 54, name: "Rogers Bartell IV",
+  #                                     vin: "1C9EVBRM0YBC564DZ")
+  #     trip = RideShare::Trip.new(id: 8, driver: @driver, passenger: nil,
+  #                                date: Time.parse("2016-08-08"), rating: 5)
+  #     @driver.add_trip(trip)
+  #   end
+  #
+  #   it "returns a float" do
+  #     expect(@driver.average_rating).must_be_kind_of Float
+  #   end
+  #
+  #   it "returns a float within range of 1.0 to 5.0" do
+  #     average = @driver.average_rating
+  #     expect(average).must_be :>=, 1.0
+  #     expect(average).must_be :<=, 5.0
+  #   end
+  #
+  #   it "returns zero if no trips" do
+  #     driver = RideShare::Driver.new(id: 54, name: "Rogers Bartell IV",
+  #                                    vin: "1C9EVBRM0YBC564DZ")
+  #     expect(driver.average_rating).must_equal 0
+  #   end
+  #
+  #   it "correctly calculates the average rating" do
+  #     trip2 = RideShare::Trip.new(id: 8, driver: @driver, passenger: nil,
+  #                                 date: Time.parse("2016-08-08"), rating: 1)
+  #     @driver.add_trip(trip2)
+  #
+  #     expect(@driver.average_rating).must_be_close_to (5.0 + 1.0) / 2.0, 0.01
+  #   end
+  #
+  #
+  # end
+  #
+  # describe "total_revenue" do
+  #   # You add tests for the total_revenue method
+  # end
+  #
+  # describe "net_expenditures" do
+  #   # You add tests for the net_expenditures method
+  # end
 end
