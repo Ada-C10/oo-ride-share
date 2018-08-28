ba37381c11812de7f0a4ff56671e825b2b854664
Laura <larmartinez2@gmail.com>
2018-08-28 15:30:33 -0700

    created test to verify that raise argument works when end date is before start date

--- specs/trip_spec.rb
+++ specs/trip_spec.rb
@@ -41,5 +41,14 @@ describe "Trip class" do
         }.must_raise ArgumentError
       end
     end
+
+    it "raises an error when end time is before start time" do
+      @trip_data[:start_time] = Time.parse('2015-05-20T12:14:00+00:00')
+      @trip_data[:end_time] = Time.parse('2015-05-10T12:14:00+00:00')
+      #binding.pry
+      expect{
+          RideShare::Trip.new(@trip_data)
+      }.must_raise ArgumentError
+    end
   end
 end
