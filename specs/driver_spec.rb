65ebf5fc6e0a4a5916455b0c79a495b5e0fca447
Madaleine Shields <maddiev.shields@gmail.com>
2018-08-29 12:50:47 -0700

    Put total_revenue tests back in spec

--- specs/driver_spec.rb
+++ specs/driver_spec.rb
@@ -2,7 +2,7 @@ require_relative 'spec_helper'
 
 describe "Driver class" do
 
-  xdescribe "Driver instantiation" do
+  describe "Driver instantiation" do
     before do
       @driver = RideShare::Driver.new(id: 54, name: "Rogers Bartell IV",
         vin: "1C9EVBRM0YBC564DZ",
@@ -98,61 +98,85 @@ describe "Driver class" do
 
             end
 
-            xdescribe "total_revenue" do
-              # You add tests for the total_revenue method
-            end
-
-            describe "net_expenditures" do
+            describe "total_revenue" do
               before do
                 @driver = RideShare::Driver.new(id: 54, name: "Rogers Bartell IV",
                   vin: "1C9EVBRM0YBC564DZ")
-
-                trip_1 = RideShare::Trip.new(id: 8, driver: nil, passenger: @driver,
-                  start_time: Time.parse("2016-08-08"),
-                  end_time: Time.parse("2016-08-09"),
-                  cost: 10.25,
-                  rating: 5)
-
-                trip_2 = RideShare::Trip.new(id: 9, driver: nil, passenger: @driver,
-                  start_time: Time.parse("2016-08-09"),
-                  end_time: Time.parse("2016-08-10"),
-                  cost: 23.45,
-                  rating: 5)
-
-                trip_3 = RideShare::Trip.new(id: 10, driver: @driver, passenger: nil,
-                    start_time: Time.parse("2016-08-10"),
-                    end_time: Time.parse("2016-08-11"),
-                    cost: 15.75,
+                  trip_01 = RideShare::Trip.new(id: 8, driver: @driver, passenger: nil,
+                    start_time: Time.parse("2016-08-08"),
+                    end_time: Time.parse("2016-08-08"),
+                    cost: 22,
                     rating: 5)
+                    @driver.add_driven_trip(trip_01)
+                    trip_02 = RideShare::Trip.new(id: 8, driver: @driver, passenger: nil,
+                      start_time: Time.parse("2016-08-09"),
+                      end_time: Time.parse("2016-08-09"),
+                      cost: 10.50,
+                      rating: 5)
+                      @driver.add_driven_trip(trip_02)
+                    end
+                    it "returns 0 if the driver has made no trips" do
+                      driver = RideShare::Driver.new(id: 54, name: "Rogers Bartell IV",
+                        vin: "1C9EVBRM0YBC564DZ")
+                        expect(driver.total_revenue).must_equal 0
+                      end
+                      it "returns the total revenue a driver has made" do
+                        expect(@driver.total_revenue).must_equal 23.36
+                      end
+                    end
 
-                trip_4 = RideShare::Trip.new(id: 11, driver: @driver, passenger: nil,
-                    start_time: Time.parse("2016-08-11"),
-                    end_time: Time.parse("2016-08-12"),
-                    cost: 3.42,
-                    rating: 5)
 
-                @driver.add_trip(trip_1)
-                @driver.add_trip(trip_2)
-                @driver.add_driven_trip(trip_3)
-                @driver.add_driven_trip(trip_4)
+                    describe "net_expenditures" do
+                      before do
+                        @driver = RideShare::Driver.new(id: 54, name: "Rogers Bartell IV",
+                          vin: "1C9EVBRM0YBC564DZ")
 
-              end
+                          trip_1 = RideShare::Trip.new(id: 8, driver: nil, passenger: @driver,
+                            start_time: Time.parse("2016-08-08"),
+                            end_time: Time.parse("2016-08-09"),
+                            cost: 10.25,
+                            rating: 5)
 
-              it 'calculates the total expenditures by a Driver' do
-                expect(@driver.net_expenditures).must_equal 21.00
-              end
+                            trip_2 = RideShare::Trip.new(id: 9, driver: nil, passenger: @driver,
+                              start_time: Time.parse("2016-08-09"),
+                              end_time: Time.parse("2016-08-10"),
+                              cost: 23.45,
+                              rating: 5)
 
-              it 'returns a negative number when total revenue is greater than gross expenditures' do
+                              trip_3 = RideShare::Trip.new(id: 10, driver: @driver, passenger: nil,
+                                start_time: Time.parse("2016-08-10"),
+                                end_time: Time.parse("2016-08-11"),
+                                cost: 15.75,
+                                rating: 5)
 
-                trip_5 = RideShare::Trip.new(id: 12, driver: @driver, passenger: nil,
-                  start_time: Time.parse("2016-08-13"),
-                  end_time: Time.parse("2016-08-14"),
-                  cost: 35.50,
-                  rating: 5)
+                                trip_4 = RideShare::Trip.new(id: 11, driver: @driver, passenger: nil,
+                                  start_time: Time.parse("2016-08-11"),
+                                  end_time: Time.parse("2016-08-12"),
+                                  cost: 3.42,
+                                  rating: 5)
 
-                @driver.add_driven_trip(trip_5)
+                                  @driver.add_trip(trip_1)
+                                  @driver.add_trip(trip_2)
+                                  @driver.add_driven_trip(trip_3)
+                                  @driver.add_driven_trip(trip_4)
 
-                expect(@driver.net_expenditures).must_equal -6.08
-              end
-            end
-          end
+                                end
+
+                                it 'calculates the total expenditures by a Driver' do
+                                  expect(@driver.net_expenditures).must_equal 21.00
+                                end
+
+                                it 'returns a negative number when total revenue is greater than gross expenditures' do
+
+                                  trip_5 = RideShare::Trip.new(id: 12, driver: @driver, passenger: nil,
+                                    start_time: Time.parse("2016-08-13"),
+                                    end_time: Time.parse("2016-08-14"),
+                                    cost: 35.50,
+                                    rating: 5)
+
+                                    @driver.add_driven_trip(trip_5)
+
+                                    expect(@driver.net_expenditures).must_equal -6.08
+                                  end
+                                end
+                              end
