383b867ffe8752b460ad8eef7eb229032e47b000
Kat Perreira <kat.perreira@gmail.com>
2018-08-29 14:42:27 -0700

    driver class created and instance test passed

--- lib/driver.rb
+++ lib/driver.rb
@@ -1,11 +1,17 @@
+#require 'pry'
+
 module RideShare
-  class Driver
+  class Driver < User
     attr_reader :vehicle_id, :driven_trips
     attr_accessor :status
 
-    
-
-
+    def initialize(data)
+      # super(id, name, phone_number, trips)
+      @vehicle_id = data[:vin]
+      @driven_trips = data[:driven_trips]
+      @status = data[:status]
+    #  binding.pry
+    end
 
   end
 end
