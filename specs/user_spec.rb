1e971a26cb08b99a146ffa9cc780bd38850807e0
Madaleine Shields <maddiev.shields@gmail.com>
2018-08-29 12:29:38 -0700

    Refactor before do and rearrange the indentation

--- specs/user_spec.rb
+++ specs/user_spec.rb
@@ -1,25 +1,55 @@
 require_relative 'spec_helper'
 
-xdescribe "User class" do
+describe "User class" do
+  before do
+    @user = RideShare::User.new(
+      id: 9,
+      name: "Merl Glover III",
+      phone: "1-602-620-2330 x3723",
+      trips: []
+    )
+
+    trip_1 = RideShare::Trip.new(
+      id: 8,
+      driver: nil,
+      passenger: @user,
+      start_time: Time.parse("2016-08-08"),
+      end_time: Time.parse("2016-08-09"),
+      cost: 10.25,
+      rating: 5
+    )
+
+    trip_2 = RideShare::Trip.new(
+      id: 9,
+      driver: nil,
+      passenger: @user,
+      start_time: Time.parse("2016-08-09"),
+      end_time: Time.parse("2016-08-10"),
+      cost: 23.45,
+      rating: 5
+    )
+
+    @user.add_trip(trip_1)
+    @user.add_trip(trip_2)
+  end
 
   describe "User instantiation" do
-    before do
-      @user = RideShare::User.new(id: 1, name: "Smithy", phone: "353-533-5334")
-    end
-
     it "is an instance of User" do
       expect(@user).must_be_kind_of RideShare::User
     end
 
     it "throws an argument error with a bad ID value" do
-      expect do
-        RideShare::User.new(id: 0, name: "Smithy")
-      end.must_raise ArgumentError
+      expect{ RideShare::User.new(id: 0, name: "Smithy") }.must_raise ArgumentError
     end
 
     it "sets trips to an empty array if not provided" do
-      expect(@user.trips).must_be_kind_of Array
-      expect(@user.trips.length).must_equal 0
+      user = RideShare::User.new(
+        id: 9,
+        name: "Merl Glover III",
+        phone: "1-602-620-2330 x3723"
+      )
+      expect(user.trips).must_be_kind_of Array
+      expect(user.trips.length).must_equal 0
     end
 
     it "is set up for specific attributes and data types" do
@@ -34,90 +64,29 @@ xdescribe "User class" do
     end
   end
 
-
   describe "trips property" do
-    before do
-      @user = RideShare::User.new(id: 9, name: "Merl Glover III",
-        phone: "1-602-620-2330 x3723", trips: [])
-        trip = RideShare::Trip.new(id: 8, driver: nil, passenger: @user,
-          start_time: Time.parse("2016-08-08"),
-          end_time: Time.parse("2016-08-09"),
-          rating: 5)
-
-          @user.add_trip(trip)
-        end
-
-        it "each item in array is a Trip instance" do
-          @user.trips.each do |trip|
-            expect(trip).must_be_kind_of RideShare::Trip
-          end
-        end
-
-        it "all Trips must have the same passenger's user id" do
-          @user.trips.each do |trip|
-            expect(trip.passenger.id).must_equal 9
-          end
-        end
+    it "each item in array is a Trip instance" do
+      @user.trips.each do |trip|
+        expect(trip).must_be_kind_of RideShare::Trip
       end
+    end
 
-      describe '#net_expenditures method' do
+    it "all Trips must have the same passenger's user id" do
+      @user.trips.each do |trip|
+        expect(trip.passenger.id).must_equal 9
+      end
+    end
+  end
 
+  describe '#net_expenditures method' do
+    it 'calculates the total expenditures by a User' do
+      expect(@user.net_expenditures).must_equal 33.70
+    end
+  end
 
-
-
-
-
-
-
-        before do
-          @user = RideShare::User.new(id: 1, name: "Smithy", phone: "353-533-5334")
-
-          trip_1 = RideShare::Trip.new(id: 8, driver: nil, passenger: @user,
-            start_time: Time.parse("2016-08-08"),
-            end_time: Time.parse("2016-08-09"),
-            cost: 10.25,
-            rating: 5)
-
-            trip_2 = RideShare::Trip.new(id: 9, driver: nil, passenger: @user,
-              start_time: Time.parse("2016-08-09"),
-              end_time: Time.parse("2016-08-10"),
-              cost: 23.45,
-              rating: 5)
-
-              @user.add_trip(trip_1)
-              @user.add_trip(trip_2)
-
-            end
-
-            it 'calculates the total expenditures by a User' do
-              expect(@user.net_expenditures).must_equal 33.70
-            end
-          end
-
-          describe '#total_time_spent method' do
-            before do
-              @user = RideShare::User.new(id: 1, name: "Smithy", phone: "353-533-5334")
-
-              trip_1 = RideShare::Trip.new(id: 8, driver: nil, passenger: @user,
-                start_time: Time.parse("2016-08-08"),
-                end_time: Time.parse("2016-08-09"),
-                cost: 10.25,
-                rating: 5)
-
-                trip_2 = RideShare::Trip.new(id: 9, driver: nil, passenger: @user,
-                  start_time: Time.parse("2016-08-09"),
-                  end_time: Time.parse("2016-08-10"),
-                  cost: 23.45,
-                  rating: 5)
-
-                  @user.add_trip(trip_1)
-                  @user.add_trip(trip_2)
-
-                end
-
-                it 'calculates the total time in trips by a User' do
-                  expect(@user.total_time_spent).must_equal 172800.0
-                end
-              end
-
-            end
+  describe '#total_time_spent method' do
+    it 'calculates the total time in trips by a User' do
+      expect(@user.total_time_spent).must_equal 172800.0
+    end
+  end
+end
