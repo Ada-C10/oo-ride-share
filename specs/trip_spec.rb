082685f3a74f4e48797ecd318a8df8f59a2e0063
Madaleine Shields <maddiev.shields@gmail.com>
2018-08-29 12:47:27 -0700

    refactor before do bloack and realign indentation

--- specs/trip_spec.rb
+++ specs/trip_spec.rb
@@ -1,102 +1,75 @@
 require_relative 'spec_helper'
 
-xdescribe "Trip class" do
+describe "Trip class" do
+  before do
+    start_time = Time.parse('2015-05-20T12:14:00+00:00')
+    end_time = start_time + 25 * 60 # 25 minutes
+    @trip_data = {
+      id: 8,
+      passenger: RideShare::User.new(
+        id: 1,
+        name: "Ada",
+        phone: "412-432-7640"
+      ),
+      driver: RideShare::Driver.new(
+        id: 2,
+        vin: '12345678912345678',
+        status: :AVAILABLE
+      ),
+      start_time: start_time,
+      end_time: end_time,
+      cost: 23.45,
+      rating: 3
+    }
+    @trip = RideShare::Trip.new(@trip_data)
+  end
 
   describe "initialize" do
-    before do
+    it "is an instance of Trip" do
+      expect(@trip).must_be_kind_of RideShare::Trip
+    end
+
+    it "stores an instance of user" do
+      expect(@trip.passenger).must_be_kind_of RideShare::User
+    end
+
+    it "stores an instance of driver" do
+      expect(@trip.driver).must_be_kind_of RideShare::Driver
+    end
+
+    it "raises an error for an invalid rating" do
+      [-3, 0, 6].each do |rating|
+        @trip_data[:rating] = rating
+        expect {
+          RideShare::Trip.new(@trip_data)
+        }.must_raise ArgumentError
+      end
+    end
+
+    it "raises an error if end time precedes start time" do
       start_time = Time.parse('2015-05-20T12:14:00+00:00')
-      end_time = start_time + 25 * 60 # 25 minutes
-      @trip_data = {
+      end_time = start_time - 25 * 60 # 25 minutes
+      trip_data = {
         id: 8,
-        passenger: RideShare::User.new(id: 1,
+        passenger: RideShare::User.new(
+          id: 1,
           name: "Ada",
-          phone: "412-432-7640"),
-          start_time: start_time,
-          end_time: end_time,
-          cost: 23.45,
-          rating: 3
-        }
-        @trip = RideShare::Trip.new(@trip_data)
-      end
+          phone: "412-432-7640"
+        ),
+        start_time: start_time,
+        end_time: end_time,
+        cost: 23.45,
+        rating: 3
+      }
+      expect {
+        trip = RideShare::Trip.new(trip_data)
+      }.must_raise ArgumentError
+    end
+  end
 
-      it "is an instance of Trip" do
-        expect(@trip).must_be_kind_of RideShare::Trip
-      end
-
-      it "stores an instance of user" do
-        expect(@trip.passenger).must_be_kind_of RideShare::User
-      end
-
-      it "stores an instance of driver" do
-
-        start_time = Time.parse('2015-05-20T12:14:00+00:00')
-        end_time = start_time + 25 * 60 # 25 minutes
-        @trip_data = {
-          id: 8,
-          passenger: RideShare::User.new(id: 1,
-            name: "Ada",
-            phone: "412-432-7640"),
-            driver: RideShare::Driver.new(id: 2,
-              vin: '12345678912345678',
-              status: :AVAILABLE
-            ),
-            start_time: start_time,
-            end_time: end_time,
-            cost: 23.45,
-            rating: 3
-          }
-          @trip = RideShare::Trip.new(@trip_data)
-
-          expect(@trip.driver).must_be_kind_of RideShare::Driver
-
-        end
-
-        it "raises an error for an invalid rating" do
-          [-3, 0, 6].each do |rating|
-            @trip_data[:rating] = rating
-            expect {
-              RideShare::Trip.new(@trip_data)
-            }.must_raise ArgumentError
-          end
-        end
-
-        it "raises an error if end time precedes start time" do
-          start_time = Time.parse('2015-05-20T12:14:00+00:00')
-          end_time = start_time - 25 * 60 # 25 minutes
-          @trip_data = {
-            id: 8,
-            passenger: RideShare::User.new(id: 1,
-              name: "Ada",
-              phone: "412-432-7640"),
-              start_time: start_time,
-              end_time: end_time,
-              cost: 23.45,
-              rating: 3
-            }
-            expect {
-              @trip = RideShare::Trip.new(@trip_data)
-            }.must_raise ArgumentError
-          end
-        end
-
-        describe "#duration method" do
-
-          it "Finds difference in seconds between end_time and start_time" do
-            start_time = Time.parse('2015-05-20T12:14:00+00:00')
-            end_time = start_time + 25 * 60 # 25 minutes
-            @trip_data = {
-              id: 8,
-              passenger: RideShare::User.new(id: 1,
-                name: "Ada",
-                phone: "412-432-7640"),
-                start_time: start_time,
-                end_time: end_time,
-                cost: 23.45,
-                rating: 3
-              }
-              @trip = RideShare::Trip.new(@trip_data)
-
-              expect(@trip.duration).must_equal 1500
-            end
-          end
-        end
+  describe "#duration method" do
+    it "Finds difference in seconds between end_time and start_time" do
+      expect(@trip.duration).must_equal 1500
+    end
+  end
+end
