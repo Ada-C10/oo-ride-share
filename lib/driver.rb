e8af14f897994a1d4be466077355968acc535663
Valerie Gidzinski <valgidzi@gmail.com>
2018-08-29 12:38:03 -0700

    Add lib/driver.rb to spec_helper.rb and initialize method for Driver class works

--- lib/driver.rb
+++ lib/driver.rb
@@ -4,25 +4,37 @@ require 'time'
 
 module RideShare
   class Driver < User
-    attr_reader :vin
-    attr_accessor :status, :driven_trips  #:trips is the trips taken as a user
+    attr_reader :vehicle_id, :status, :driven_trips  #:trips is the trips taken as a user
 
     def initialize(input) #rather than input, pass in symbol vin, status, and driven trips
-      super (input :id], input[:name], input[:phone_number], input[:trips])
-
-
-      if input[:id].nil? || input[:id] <= 0
-        raise ArgumentError, 'ID cannot be blank or less than zero.'
-      end
-
-      if input[:vin].length != 17
-        raise ArgumentError, 'vin numbers must be 17 characters in length'
-      end
-
+      super(input)
+      # inherits @id, @name, @phone as param and inst var (?)
+      # inherits @trips from User (?) as instance variable not parameter
       # rather than input[:vin], just :vin
-      @vin = input[:vin]
-      @status = input[:status]
-      @driven_trips = input[:driven_trips]
+      @vehicle_id = input[:vin]
+      # if input[:vehicle_id].nil? || input[:vehicle_id] <= 0
+      #   raise ArgumentError, 'ID cannot be blank or less than zero.'
+      # end
+      #
+      # if input[:vin].length != 17
+      #   raise ArgumentError, 'vin numbers must be 17 characters in length'
+      # end
+
+      @status = status
+      # conditional for two status options
+
+      # @driven_trips = driven_trips[:trips].nil? [] : driven_trips[:trips]
+      # @driven_trips =
+      # if driven_trips[:trips].nil?
+      #   @driven_trips = []
+      # else
+      #   @driven_trips = driven_trips[:trips]
+      # end
+      @driven_trips = input[:trips].nil? ? [] : input[:trips]
+
+
+
+
 
     end
 
