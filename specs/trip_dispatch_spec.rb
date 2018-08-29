560ca55f724f4c53b255c4eb7b0de44d3571b2ab
Chantelle Belic <chantelle.belic@gmail.com>
2018-08-29 16:41:24 -0700

    merging

--- specs/trip_dispatch_spec.rb
+++ specs/trip_dispatch_spec.rb
@@ -67,6 +67,7 @@ describe "TripDispatcher class" do
     end
 
     it "accurately loads driver information into drivers array" do
+      skip
       # Unskip After Wave 2
       first_driver = @dispatcher.drivers.first
       last_driver = @dispatcher.drivers.last
@@ -80,6 +81,7 @@ describe "TripDispatcher class" do
     end
 
     it "Connects drivers with trips" do
+      skip
      # Unskip after wave 2
       trips = @dispatcher.trips
 
