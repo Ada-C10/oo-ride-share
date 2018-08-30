eba9dee8e368ab9f30d526213e78fd4b44740b4f
Maryam Shitu <maryamshitu@gmail.com>
2018-08-30 13:54:21 -0700

    adding Driver#add_trip

--- lib/driver.rb
+++ lib/driver.rb
@@ -15,5 +15,16 @@ module RideShare
         @status = input[:status] ||= :AVAILABLE
         raise ArgumentError.new("Invalid Status") unless @status == :AVAILABLE || @status == :UNAVAILABLE
     end
+
+    def add_driven_trip(trip)
+      check_id(trip.id)
+      @driven_trips << super(trip) 
+
+    end
+
+    def check_id(id)
+      raise ArgumentError, "ID cannot be blank or less than zero. (got #{id})" if id.nil? || id <= 0
+    end
+
   end
 end
