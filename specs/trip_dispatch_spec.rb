18918ef133f5a2f591e62ca24a9446178a3f466f
Cassandra Archibald <cassandra.l.archibald@gmail.com>
2018-08-30 14:21:37 -0700

    Refactored spec for trip_dispatch

--- specs/trip_dispatch_spec.rb
+++ specs/trip_dispatch_spec.rb
@@ -1,14 +1,12 @@
 require_relative 'spec_helper'
 require 'time'
 
-USER_TEST_FILE   = 'specs/test_data/users_test.csv'
-TRIP_TEST_FILE   = 'specs/test_data/trips_test.csv'
-DRIVER_TEST_FILE = 'specs/test_data/drivers_test.csv'
 describe "TripDispatcher class" do
   before do
-    @dispatcher = RideShare::TripDispatcher.new(USER_TEST_FILE,
-                                               TRIP_TEST_FILE,
-                                                DRIVER_TEST_FILE)
+    USER_TEST_FILE   = 'specs/test_data/users_test.csv'
+    TRIP_TEST_FILE   = 'specs/test_data/trips_test.csv'
+    DRIVER_TEST_FILE = 'specs/test_data/drivers_test.csv'
+    @dispatcher = RideShare::TripDispatcher.new(USER_TEST_FILE, TRIP_TEST_FILE, DRIVER_TEST_FILE)
   end
 
   describe "Initializer" do
@@ -17,11 +15,9 @@ describe "TripDispatcher class" do
     end
 
     it "establishes the base data structures when instantiated" do
-      @dispatcher = RideShare::TripDispatcher.new
       [:trips, :passengers].each do |prop|
         expect(@dispatcher).must_respond_to prop
       end
-
       expect(@dispatcher.trips).must_be_kind_of Array
       expect(@dispatcher.passengers).must_be_kind_of Array
       expect(@dispatcher.drivers).must_be_kind_of Array
@@ -29,20 +25,14 @@ describe "TripDispatcher class" do
   end
 
   describe "find_user method" do
-    before do
-      @dispatcher = RideShare::TripDispatcher.new
-    end
-
     it "throws an argument error for a bad ID" do
       expect{ @dispatcher.find_passenger(0) }.must_raise ArgumentError
     end
 
     it "finds a user instance" do
-      passenger = @dispatcher.find_passenger(2)
-      expect(passenger).must_be_kind_of RideShare::User
+      expect(@dispatcher.find_passenger(2)).must_be_kind_of RideShare::User
     end
   end
-
 
   describe "find_driver method" do
     it "throws an argument error for a bad ID" do
@@ -50,8 +40,7 @@ describe "TripDispatcher class" do
     end
 
     it "finds a driver instance" do
-      driver = @dispatcher.find_driver(2)
-      expect(driver).must_be_kind_of RideShare::Driver
+      expect(@dispatcher.find_driver(2)).must_be_kind_of RideShare::Driver
     end
   end
 
@@ -59,7 +48,6 @@ describe "TripDispatcher class" do
     it "accurately loads driver information into drivers array" do
       first_driver = @dispatcher.drivers.first
       last_driver = @dispatcher.drivers.last
-
       expect(first_driver.name).must_equal "Driver2"
       expect(first_driver.id).must_equal 2
       expect(first_driver.status).must_equal :UNAVAILABLE
@@ -70,20 +58,22 @@ describe "TripDispatcher class" do
 
     it "Connects drivers with trips" do
       trips = @dispatcher.trips
-
       [trips.first, trips.last].each do |trip|
         driver = trip.driver
         expect(driver).must_be_instance_of RideShare::Driver
-        # expect(driver.trips).must_include trip
+        expect(driver.driven_trips).must_include trip
+        expect(trip).must_be_instance_of RideShare::Trip
       end
     end
   end
 
   describe "User & Trip loader methods" do
+    before do
+      @trip = @dispatcher.trips.first
+    end
     it "accurately loads passenger information into passengers array" do
       first_passenger = @dispatcher.passengers.first
       last_passenger = @dispatcher.passengers.last
-
       expect(first_passenger.name).must_equal "User1"
       expect(first_passenger.id).must_equal 1
       expect(last_passenger.name).must_equal "Driver8"
@@ -91,35 +81,20 @@ describe "TripDispatcher class" do
     end
 
     it "accurately loads trip info and associates trips with passengers" do
-      trip = @dispatcher.trips.first
-      passenger = trip.passenger
-
+      passenger = @trip.passenger
       expect(passenger).must_be_instance_of RideShare::User
-      expect(passenger.trips).must_include trip
+      expect(passenger.trips).must_include @trip
     end
 
     it "saves start and end time as Time objects" do
-      trip = @dispatcher.trips.first
-      trip_start_time = trip.start_time
-      trip_end_time = trip.end_time
-
+      trip_start_time = @trip.start_time
+      trip_end_time = @trip.end_time
       expect(trip_start_time).must_be_instance_of Time
       expect(trip_end_time).must_be_instance_of Time
     end
   end
 
   describe "TripDispatcher#request_trip" do
-    # this is the trip that should get created from the test data files
-    #   @trip_data = {
-    #     id: 6,
-    #     passenger: @dispatcher.find_passenger(1),
-    #     start_time: Time.now,
-    #     end_time: nil,
-    #     cost: nil,
-    #     rating: nil,
-    #     driver: @dispatcher.find_driver(5)
-    #   }
-
     it "must call an available driver" do
       expect(@dispatcher.find_driver(5).status).must_equal :AVAILABLE
       expect(@dispatcher.request_trip(1).driver).must_equal @dispatcher.find_driver(5)
@@ -130,17 +105,20 @@ describe "TripDispatcher class" do
     end
 
     it "must return the correct trip information" do
+      def change_status_to_available(driver_id)
+        @dispatcher.find_driver(driver_id).status = :AVAILABLE
+      end
       expect(@dispatcher.request_trip(1).id).must_equal 6
-      @dispatcher.find_driver(5).status = :AVAILABLE
+      change_status_to_available(5)
       expect(@dispatcher.request_trip(1).passenger).must_equal @dispatcher.find_passenger(1)
-      # expect(@dispatcher.request_trip(1).start_time).must_equal Time.now
-      @dispatcher.find_driver(5).status = :AVAILABLE
+      expect(@dispatcher.request_trip(1).start_time).must_be_close_to Time.now, 0.01
+      change_status_to_available(5)
       expect(@dispatcher.request_trip(1).end_time).must_equal nil
-      @dispatcher.find_driver(5).status = :AVAILABLE
+      change_status_to_available(5)
       expect(@dispatcher.request_trip(1).cost).must_equal nil
-      @dispatcher.find_driver(5).status = :AVAILABLE
+      change_status_to_available(5)
       expect(@dispatcher.request_trip(1).rating).must_equal nil
-      @dispatcher.find_driver(5).status = :AVAILABLE
+      change_status_to_available(5)
       expect(@dispatcher.request_trip(1).driver).must_equal @dispatcher.find_driver(5)
     end
 
@@ -166,9 +144,7 @@ describe "TripDispatcher class" do
       # set all drivers to unavailable
       @dispatcher.find_driver(5).status = :UNAVAILABLE
       @dispatcher.find_driver(8).status = :UNAVAILABLE
-
       expect{@dispatcher.request_trip(1)}.must_raise ArgumentError
     end
-
   end
 end
