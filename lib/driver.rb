5fd7d4987bf0ad1c5cdb8c15eeddf21a0d7546a4
Katricia Smith <smithkatricia@gmail.com>
2018-08-30 17:10:18 -0700

    added super with parameter

--- lib/driver.rb
+++ lib/driver.rb
@@ -6,10 +6,20 @@ module RideShare
     attr_accessor :status
 
     def initialize(data)
-      # super(id, name, phone_number, trips)
+      super(data)
+      # @id = data[:id]
       @vehicle_id = data[:vin]
-      @driven_trips = data[:driven_trips]
+      @driven_trips = data[:driven_trips].nil? ? [] : data[:driven_trips]
       @status = data[:status]
+
+    # def initialize(id, name, phone_number, trips, vehicle_id, driven_trips, status)
+    #   super(id, name, phone_number, trips)
+    #     @vehicle_id = vehicle_id
+    #     @driven_trips =  drivers[:driven_trips].nil? ? [] : drivers[:driven_trips]
+    #     @status = status
+
+
+# use find passenger and merge with user to make one hash and pass into constructor?
 
 
       good_status = [:AVAILABLE, :UNAVAILABLE]
@@ -17,8 +27,14 @@ module RideShare
       unless good_status.include? @status
         raise ArgumentError.new("That status is not recognized")
       end
-        #  binding.pry
-      end
+    end # end of def initialize
 
+    def add_driven_trip(trip)
+      # @trips.each do |trip|
+      #   if Trip.id
+      #
+      @driven_trips << trip
     end
-  end
+
+  end # end of Class User
+end # end of Module
