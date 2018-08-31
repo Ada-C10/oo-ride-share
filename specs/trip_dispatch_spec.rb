cdf15cd3bf31b37a95ef3d45982e000841e34b8c
Maryam Shitu <maryamshitu@gmail.com>
2018-08-31 16:42:04 -0700

    commiting so that pair can be updated

--- specs/trip_dispatch_spec.rb
+++ specs/trip_dispatch_spec.rb
@@ -139,9 +139,9 @@ describe "TripDispatcher class" do
       expect(@dispatcher.request_trip(1).driver.status).must_equal :AVAILABLE
     end
 
-    it "returns a driver who is AVAILABLE" do
-      expect{}(@dispatcher.request_trip(1).driver.status).must_raise ArgumentError
-    end
+    # it "returns a driver who is AVAILABLE" do
+    #   expect{}(@dispatcher.request_trip(1).driver.status).must_raise ArgumentError
+    # end
 
 
     # it "will add trip to trips array" do
