1eaacc6c920b5c3d2bdf73bb3837f51325fdc6d2
Layla <layla.bristol@gmail.com>
2018-08-31 10:58:48 -0700

    adding two checks to initilize

--- lib/driver.rb
+++ lib/driver.rb
@@ -5,16 +5,25 @@ module RideShare
   class Driver < User
     attr_reader :vin, :status, :driven_trips
     VALID_STATUSES = [:AVAILABLE, :UNAVAILABLE]
+
     def initialize(input)
 
-      unless VALID_STATUSES.include?(input[:status])
-        raise ArgumentError
-      end
+      if VALID_STATUSES.include?(input[:status]) == false
+       raise ArgumentError
+      else input[:status] == nil
+        @status = :UNAVAILABLE
+     end
 
+     if input[:vin].length != 17
+            raise ArgumentError
+          end
+          
       super (input)
       @vin = input[:vin]
       @status = input[:status]
       @driven_trips = input[:trips].nil? ? [] : input[:trips]
+
+
     end
 
     def add_driven_trip(trip)
@@ -25,7 +34,7 @@ module RideShare
         @status = :UNAVAILABLE
       end
       @driven_trips << trip
-    
+
     end
 
 
