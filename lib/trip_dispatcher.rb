9d724f7fd19c7278f5998c50d7bc7e350782ddb5
Daniela Di Iorio <danielad@gmail.com>
2018-08-31 12:50:16 -0700

    refactored trip_dispatcher.rb

--- lib/trip_dispatcher.rb
+++ lib/trip_dispatcher.rb
@@ -15,37 +15,36 @@ module RideShare
       @passengers = load_users(user_file)
       @drivers = load_drivers(driver_file)
       @trips = load_trips(trip_file)
-
     end
 
     def load_users(filename)
       users = []
+
       CSV.read(filename, headers: true).each do |line|
         users << User.new({:id => line[0].to_i, :name => line[1], :phone => line[2]})
       end
+
       return users
     end
 
     def load_drivers(filename)
-
       drivers = []
 
       CSV.read(filename, headers: true).each do |line|
         found_passenger = find_passenger(line[0].to_i)
 
-        new_driver = Driver.new(id: line[0].to_i, name: found_passenger.name,
-                                phone: found_passenger.phone_number, vin: line[1],
-                                status: line[2].to_sym, trips: found_passenger.trips)
+        new_driver = Driver.new(id: line[0].to_i,
+                                name: found_passenger.name,
+                                phone: found_passenger.phone_number,
+                                vin: line[1],
+                                status: line[2].to_sym,
+                                trips: found_passenger.trips)
 
         drivers << new_driver
-
         @passengers[@passengers.index(found_passenger)] = new_driver
-
       end
 
       return drivers
-
-        #Load the Drivers from the support/drivers.csv file and return a collection of Driver instances, note that drivers can be passengers too! Replace the instance of User in the passengers array with a cooresponding instance of Driver
     end
 
     def find_driver(id)
@@ -54,9 +53,8 @@ module RideShare
     end
 
     def load_trips(filename)
-#This method should be updated to add a corresponding Driver to the trip instance.
-
       trips = []
+
       trip_data = CSV.open(filename, 'r', headers: true,
                                           header_converters: :symbol)
 
@@ -96,26 +94,29 @@ module RideShare
     end
 
     def request_trip(user_id)
-      # automatically assign a driver to trip
-      # choose first driver whose status is :AVAILABLE
-      # driver cannot drive themselves (driver cannot have same ID as user)
-      # use current time for start time
-      # end date, cost, rating will all be nil
-      available_drivers = @drivers.select { |driver| driver.status == :AVAILABLE and driver.id != user_id }
+      available_drivers = @drivers.select {|driver| driver.status == :AVAILABLE and driver.id != user_id }
+
       driver = available_drivers.find {|driver| driver.driven_trips.empty?}
 
+      # first find the latest trip end time for each driver, then find the driver associated with the least recent trip end time
       driver ||= available_drivers.min_by {|driver|
-
         latest_trip = driver.driven_trips.max_by {|trip| trip.end_time}
-
         latest_trip.end_time}
-
 
       raise ArgumentError, 'No drivers available' if driver == nil
 
       passenger = find_passenger(user_id)
 
-      input = {id: @trips.last.id + 1, driver: driver, passenger: passenger, start_time: Time.now, end_time: nil, cost: nil, rating: nil}
+      input = {
+        id: @trips.last.id + 1,
+        driver: driver,
+        passenger: passenger,
+        start_time: Time.now,
+        end_time: nil,
+        cost: nil,
+        rating: nil
+      }
+
       new_trip = Trip.new(input)
       @trips << new_trip
 
@@ -125,15 +126,7 @@ module RideShare
       passenger.add_trip(new_trip)
 
       return new_trip
-
     end
-
-
-
-
-
-
-
 
     private
 
@@ -142,9 +135,3 @@ module RideShare
     end
   end
 end
-
-# USER_TEST_FILE   = 'specs/test_data/users_test.csv'
-# TRIP_TEST_FILE   = 'specs/test_data/trips_test.csv'
-# DRIVER_TEST_FILE = 'specs/test_data/drivers_test.csv'
-#
-# RideShare::TripDispatcher
