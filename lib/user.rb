e2b7de419a2d77c2bf21364348b8c8d8c4066ff8
Trang <trang.frego@gmail.com>
2018-08-31 13:03:03 -0700

    refactor user.rb

--- lib/user.rb
+++ lib/user.rb
@@ -20,21 +20,15 @@ module RideShare
     def net_expenditures
       total = 0
       @trips.each do |trip|
-        unless trip.end_time == nil
-          total += trip.cost
-        end
+        total += trip.cost unless trip.end_time == nil
       end
       return total
-      # Returns the total amount of money that user has spent on their trips
     end
 
     def total_time_spent
-      # Returns the total amount of time that user has spent on their trips
       total_time = 0
       @trips.each do |trip|
-        unless trip.end_time == nil
-          total_time += trip.trip_duration
-        end
+        total_time += trip.trip_duration unless trip.end_time == nil
       end
       return total_time
     end
@@ -47,11 +41,11 @@ module RideShare
     def initialize(id: 0, name: "no name", vin: 0, phone: 0, status: :UNAVAILABLE, trips: [])
       raise ArgumentError.new "ID not valid" if id <= 0
       raise ArgumentError.new "Invalid VIN" if vin.length != 17
+      raise ArgumentError.new "Invalid status" if status != :AVAILABLE && status != :UNAVAILABLE
       @id = id
       @name = name
       @vehicle_id = vin
       @phone = phone
-      raise ArgumentError.new "Invalid status" if status != :AVAILABLE && status != :UNAVAILABLE
       @status = status
       @driven_trips = []
       @trips = trips
@@ -60,35 +54,28 @@ module RideShare
     def average_rating
       sum = 0
       in_progress = 0
+
       @driven_trips.each do |trip|
-        if trip.end_time == nil
-          in_progress += 1
-        else
-          sum += trip.rating
-        end
+        trip.end_time == nil ? in_progress += 1 : sum += trip.rating
       end
+
       valid_trips = @driven_trips.length - in_progress
-      if valid_trips == 0
-        return 0
-      else
-        return sum.to_f/valid_trips
-      end
+
+      return valid_trips == 0 ? 0 : sum.to_f/valid_trips
     end
 
     def add_driven_trip(trip)
-      #This method adds a trip to the driver's collection of trips for which they have acted as a driver
       raise ArgumentError, "Invalid trip object" unless trip.instance_of?(Trip)
       @driven_trips << trip
     end
 
     def total_revenue
-      #This method calculates that driver's total revenue across all their trips. Each driver gets 80% of the trip cost after a fee of $1.65 per trip is subtracted.
       sum = 0
+
       @driven_trips.each do |driven_trip|
-        unless driven_trip.end_time == nil
-          sum += (driven_trip.cost - 1.65) * 0.8
-        end
+        sum += (driven_trip.cost - 1.65) * 0.8 unless driven_trip.end_time == nil
       end
+
       return sum.round(2)
     end
 
@@ -100,6 +87,4 @@ module RideShare
       @status = :UNAVAILABLE
     end
   end
-
-
 end
